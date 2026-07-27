8cd426834b464c9a96c33835d3455d2d61f20f63
singhrishav16024321-spec <singhrishav16024321@gmail.com>
2026-07-27 21:32:15 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (24.39%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int count=0;int n= nums.size();
+        sort(nums.begin(),nums.end());
+        if(n==1){return nums[0];}
+        for(int i=0;i<n;i++){
+            if(nums[i]==nums[i+1]){
+                count++;
+            }
+            else if(nums[i]!=nums[i+1]&&count==1){
+                count=0;
+            }
+            else if(nums[i]!=nums[i+1]&&count==0){
+                return nums[i];
+            }
+        }
+        return 0;
+      
+    }
+};
