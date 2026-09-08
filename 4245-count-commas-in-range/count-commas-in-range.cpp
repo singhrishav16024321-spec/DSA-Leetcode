533660580dcbaae8bf85f286eadc41f5c1f9795b
singhrishav16024321-spec <singhrishav16024321@gmail.com>
2026-09-09 02:32:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.72%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        if (n<1000){
+            return 0;
+        }
+        else if( n>=1000 && n<=100000){
+            return (n-1000+1);
+        }
+        return 0;
+    }
+};
