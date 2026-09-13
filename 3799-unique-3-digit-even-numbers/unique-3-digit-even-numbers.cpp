85da13030cb9b64b1bae49ab4613c5d879739762
singhrishav16024321-spec <singhrishav16024321@gmail.com>
2026-09-13 14:36:12 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (98.02%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        int freq[10] = {};
+        for (int digit : digits) {
+            freq[digit]++;
+        }
+        int count = 0;
+        for (int a = 1; a <= 9; a++) {
+            if (freq[a] == 0)
+                continue;
+            freq[a]--;
+            for (int b = 0; b <= 9; b++) {
+                if (freq[b] == 0)
+                    continue;
+                freq[b]--;
+                for (int c = 0; c <= 8; c += 2) {
+                    if (freq[c] == 0)
+                        continue;
+                    count++;
+                }
+                freq[b]++;
+            }
+            freq[a]++;
+        }
+        return count;
+    }
+};
