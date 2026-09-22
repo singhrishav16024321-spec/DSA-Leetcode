c52a03579eebc789ffa602f9cbab57671ab03119
singhrishav16024321-spec <singhrishav16024321@gmail.com>
2026-09-22 14:08:31 +0530

    Time: 10 ms (96.60%) | Memory: 60.1 MB (92.99%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+
+        if(nums.empty()) return 0;
+
+        sort(nums.begin(), nums.end());
+
+        int count = 1;
+        int maxcount = 1;
+
+        for(int i = 1; i < nums.size(); i++) {
+
+            if(nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            if(nums[i] == nums[i - 1] + 1) {
+                count++;
+            }
+            else {
+                count = 1;
+            }
+
+            maxcount = max(maxcount, count);
+        }
+
+        return maxcount;
+    }
+};
