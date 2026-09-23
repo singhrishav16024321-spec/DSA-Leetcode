09093708c773e21651946c03897c35b5bfe2d5d4
singhrishav16024321-spec <singhrishav16024321@gmail.com>
2026-09-23 22:23:34 +0530

    Time: 245 ms (5.45%) | Memory: 107.2 MB (6.63%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n= nums.size();
+        map<int,int> mpp;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+        }
+        for(auto it: mpp){
+            if(it.second >=2){
+                return it.first;
+            }
+        }
+        return 0;
+    }
+};
